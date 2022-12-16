1e0f258a4a2752a78783e8285a3c517184f57024
Misha Ship <monteship@gmail.com>
2022-12-16 07:55:28 +0200

    add superuser

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -12,7 +12,17 @@ then
 fi
 
 # Uncomment for the first build
-# python manage.py flush --no-input
-# python manage.py migrate
+python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createcachetable
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
 
 exec "$@"
